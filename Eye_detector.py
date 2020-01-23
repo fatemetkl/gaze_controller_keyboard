faf6e72ffa57f8263637c97c992e79b9cc7d0ad6
fateme tavakoli <t.fateme@gmail.com>
2020-01-23 12:44:43 +0330

    letters on keyboard light up in sequence and the keyboard screen is updated each time

--- Eye_detector.py
+++ Eye_detector.py
@@ -7,10 +7,82 @@ cap = cv2.VideoCapture(0)
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks_1.dat")
 font = cv2.FONT_HERSHEY_SIMPLEX
+keyboard = np.zeros((1000, 1500, 3), np.uint8)
 
+keys_set_1 = {0: "Q", 1: "W", 2: "E", 3: "R", 4: "T",
+              5: "A", 6: "S", 7: "D", 8: "F", 9: "G",
+              10: "Z", 11: "X", 12: "C", 13: "V", 14: "B"}
 
 def midpoint(p1 ,p2):
     return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+
+def letter(letter_index,text,light):
+    # Keys
+    if letter_index == 0:
+        x = 0
+        y = 0
+    elif letter_index == 1:
+        x = 200
+        y = 0
+    elif letter_index == 2:
+        x = 400
+        y = 0
+    elif letter_index == 3:
+        x = 600
+        y = 0
+    elif letter_index == 4:
+        x = 800
+        y = 0
+    elif letter_index == 5:
+        x = 0
+        y = 200
+    elif letter_index == 6:
+        x = 200
+        y = 200
+    elif letter_index == 7:
+        x = 400
+        y = 200
+    elif letter_index == 8:
+        x = 600
+        y = 200
+    elif letter_index == 9:
+        x = 800
+        y = 200
+    elif letter_index == 10:
+        x = 0
+        y = 400
+    elif letter_index == 11:
+        x = 200
+        y = 400
+    elif letter_index == 12:
+        x = 400
+        y = 400
+    elif letter_index == 13:
+        x = 600
+        y = 400
+    elif letter_index == 14:
+        x = 800
+        y = 400
+
+
+
+    width = 200
+    height = 200
+    th = 3  # thickness
+    if light is True:
+        cv2.rectangle(keyboard, (x + th, y + th), (x + width - th, y + height - th), (255, 255,255), -1)
+    else:
+        cv2.rectangle(keyboard, (x + th, y + th), (x + width - th, y + height - th), (255, 0, 0), th)
+    # Text settings
+    font_letter = cv2.FONT_HERSHEY_PLAIN
+    font_scale = 10
+    font_th = 4
+    text_size = cv2.getTextSize(text, font_letter, font_scale, font_th)[0]
+    width_text, height_text = text_size[0], text_size[1]
+    text_x = int((width - width_text) / 2) + x
+    text_y = int((height + height_text) / 2) + y
+    cv2.putText(keyboard, text, (text_x, text_y), font_letter, font_scale, (255, 0, 0), font_th)
+
 
 
 def get_blinking_ratio(eye_points, facial_landmarks):
@@ -69,10 +141,17 @@ def get_gaze_ratio(eye_points,landmarks):
 
     return gaze_ratio
 
+#counters
+frames=0
+letter_index=0
+
 while True:
     _, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frames+=1
+    keyboard[:]=(0,0,0) # reset keyboard for update
 
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    new_frame = np.zeros((500, 500, 3), np.uint8)
 
     faces = detector(gray)
     for face in faces:
@@ -90,7 +169,6 @@ while True:
         gaze_ratio_right_eye = get_gaze_ratio([42, 43, 44, 45, 46, 47], landmarks)
         gaze_ratio = (gaze_ratio_right_eye + gaze_ratio_left_eye) / 2
 
-        new_frame=np.zeros((500,500,3),np.uint8)
         if gaze_ratio <= 1:
             cv2.putText(frame, "RIGHT", (50, 100), font, 2, (0, 0, 255), 3)
             new_frame[:] = (0, 0, 255)
@@ -107,6 +185,22 @@ while True:
         # cv2.imshow("Eye", only_eye_big)
         # cv2.imshow("Threshold", threshold_eye)
         # cv2.imshow("Left eye", left_eye)
+
+        if frames==10:
+            letter_index+=1
+            frames=0
+        if letter_index==15:
+            letter_index=0
+
+
+        for i in range(15):
+            if i == letter_index:
+                light = True
+            else:
+                light = False
+            letter(i, keys_set_1[i], light)
+
+        cv2.imshow("keyboard", keyboard)
         cv2.imshow("new frame",new_frame)
         cv2.imshow("Frame", frame)
 
@@ -114,3 +208,5 @@ while True:
         if key == 27:
             break
 
+cap.release()
+cv2.destroyAllWindows()
